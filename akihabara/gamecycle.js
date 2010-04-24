bf9eb1ec1fd33aaf6eeeae6850de321e724c5344
Richard Ramsden <rramsden@bravenet.com>
2010-04-25 07:45:37 +0800

    updated readme, changed step to state in gamecycle

--- akihabara/gamecycle.js
+++ akihabara/gamecycle.js
@@ -3,11 +3,17 @@
 // ---
 
 /**
- * Gamecycle module provides a complete generic game cycle: intro, menus, 
+ * Gamecycle contains your basic game loop: intro, menus, 
  * crossfading between stages/lifes, gameover and ending. 
- * You can use the same game cycle for many games and implement only the main game.
  */
 var gamecycle={
+
+	/**
+	 * Gamecycle constructor - initializes a new game object
+	 * 
+	 * @param	id		unique id of object
+	 * @param	group		name of group to store the object in
+	 */
 	createMaingame:function(id,group) {
 	return gbox.addObject({
 		id:id,
@@ -15,18 +21,25 @@ var gamecycle={
 		counter:0,
 		difficulty:0,
 		
-		step:100,
-		isready:false,
+		// state transition
+		state:100,  
+		isready:false, 
 		
 		hud:{},
 		
-		// OVERRIDABLE
-		
+		/**
+		 * This method is called whenever you load a new map. It's meant to be
+		 * overridden when you create your game.
+		 */
 		changeLevel:function() { },
+		
+		/**
+		 * This method is called every time a player is "reborn". This method is 
+		 * meant to be overridden since you have to do garbage collection. 
+		 */
 		newLife:function() { },
 		
-		// Level intro animation
-		
+		// level intro animation
 		gameIntroAnimation:function(reset) {
 			if (reset) {
 				toys.resetToy(this,"default-blinker");
@@ -102,10 +115,14 @@ var gamecycle={
 			}
 		},
 		
-		// Decides if the game is over
+		/**
+		 * This method is called when the player dies.
+		 */
 		gameIsOver:function() { return true; },
 		
-		// Actions done during the game (i.e. stage is clear or other ending conditions)
+		/** 
+		 * Actions done during the game (i.e. stage is clear or other ending conditions)
+		 */
 		gameEvents:function() {
 		
 		},
@@ -129,11 +146,11 @@ var gamecycle={
 		// CHECK
 		
 		gameIsHold:function() { // Use this clause to check collision and kill player: if true the level is changing
-			return (this.step==400)||(this.step==401);
+			return (this.state==400)||(this.state==401);
 		},
 		
 		isCompleted:function() {
-			return (this.step==800);
+			return (this.state==800);
 		},
 		
 		// GAME CYCLE
@@ -144,22 +161,22 @@ var gamecycle={
 		
 		gotoLevel:function(level) {
 			this._nextlevel=level;
-			this.setStep(400);
+			this.setState(400);
 		},
 		
 		playerDied:function(data) {
 			this._loselife=data;
-			this.setStep(500);
+			this.setState(500);
 		},
 		
 		gameIsCompleted:function() {
-			this.setStep(800);
+			this.setState(800);
 		},
 		
 		// PRIVATES
 		
-		setStep:function(st) {
-			this.step=st;
+		setState:function(st) {
+			this.state=st;
 			this.isready=false;
 		},
 		
@@ -170,46 +187,46 @@ var gamecycle={
 			gbox.soloGroup(this.group);
 		},
 
-		stepIsReady:function() { this.isready=true; },
+		stateIsReady:function() { this.isready=true; },
 		blit:function() {
 			var ok=false;
-			switch (this.step) {
+			switch (this.state) {
 				case 100:
 				case 101:
 				case 102: { // Press Start / Menu
-					if (!this.isready&&(this.step==100)) {
+					if (!this.isready&&(this.state==100)) {
 						this._resetGroups();
 						this.gameTitleIntroAnimation(true);
 					}
 					this.gameTitleIntroAnimation(false);
-					switch (this.step) {
+					switch (this.state) {
 						case 100: { // Press to start
 							if (!this.isready) {
 								this.pressStartIntroAnimation(true);
-								this.stepIsReady();
+								this.stateIsReady();
 							}
-							if (this.pressStartIntroAnimation(false)) this.setStep(101);
+							if (this.pressStartIntroAnimation(false)) this.setState(101);
 							break;
 						}
 						case 101: { // Game menu
 							if (!this.isready) {
 								this.gameMenu(true);
-								this.stepIsReady();
+								this.stateIsReady();
 							}
 							
 							var menu=this.gameMenu(false);
 							if (menu)
-								if (menu == -1) this.setStep(100); else this.setStep(102);
+								if (menu == -1) this.setState(100); else this.setState(102);
 							break;
 						}
 						case 102: { // Fader
 							if (!this.isready) {
 								this._resetGroups();
 								toys.resetToy(this,"fadeout");
-								this.stepIsReady();
+								this.stateIsReady();
 							}
 							if (toys.fullscreen.fadeout(this,"fadeout",gbox.getBufferContext(),{fadespeed:0.05}))
-								this.setStep(200);
+								this.setState(200);
 							break;
 						}
 						break;
@@ -238,7 +255,7 @@ var gamecycle={
 
 				{ // Game playing
 					if (!this.isready) {
-						switch (this.step) {
+						switch (this.state) {
 							case 200: { // Game intro
 								toys.resetToy(this,"fadein");
 								this.level=null;
@@ -304,18 +321,18 @@ var gamecycle={
 								break;
 							}
 						}
-						this.stepIsReady();
+						this.stateIsReady();
 					}
 					
-					switch (this.step) {
+					switch (this.state) {
 						case 200: { // Game intro
-							if (this.gameIntroAnimation(false)) this.setStep(300);
+							if (this.gameIntroAnimation(false)) this.setState(300);
 							break;
 						}
 						case 601: // Fade in with new life
 						case 402: // Fade in after level change
 						case 300: { // Fade in at the beginning of the game
-							if (toys.fullscreen.fadein(this,"fadein",gbox.getBufferContext(),{fadespeed:0.05})) this.setStep(301);
+							if (toys.fullscreen.fadein(this,"fadein",gbox.getBufferContext(),{fadespeed:0.05})) this.setState(301);
 							break;
 						}
 						case 301: { // Ingame stuff
@@ -324,53 +341,52 @@ var gamecycle={
 						}
 						case 400: { // Fade out before changing the level
 							if (this.endlevelIntroAnimation(false))
-								if (toys.fullscreen.fadeout(this,"fadeout",gbox.getBufferContext(),{fadespeed:0.05})) this.setStep(401);
+								if (toys.fullscreen.fadeout(this,"fadeout",gbox.getBufferContext(),{fadespeed:0.05})) this.setState(401);
 							break;
 						}
 						case 800: { // Fade out before game ending
 							if (this.endlevelIntroAnimation(false))
-								if (toys.fullscreen.fadeout(this,"fadeout",gbox.getBufferContext(),{fadespeed:0.05})) this.setStep(801);
+								if (toys.fullscreen.fadeout(this,"fadeout",gbox.getBufferContext(),{fadespeed:0.05})) this.setState(801);
 							break;
 						}
 						case 501: { // Fade out after dead
 							if (toys.fullscreen.fadeout(this,"fadeout",gbox.getBufferContext(),{fadespeed:0.05})) 
 								if (this.gameIsOver())
-									this.setStep(700); // GAME OVER
+									this.setState(700); // GAME OVER
 								else
-									this.setStep(600);
+									this.setState(600);
 							break;
 						}
 						case 401:{ // Level intro animation
-							if (this.levelIntroAnimation(false)) this.setStep(402);
+							if (this.levelIntroAnimation(false)) this.setState(402);
 							break;
 						}
 						case 500: { // Wait after dead
 							this._loselife.counter++;
-							if (this._loselife.counter==this._loselife.wait) this.setStep(501);
+							if (this._loselife.counter==this._loselife.wait) this.setState(501);
 							break;
 						}
 						case 600:{ // New life intro
-							if (this.newlifeIntroAnimation(false)) this.setStep(601);
+							if (this.newlifeIntroAnimation(false)) this.setState(601);
 							break;
 						}
 						case 700:{ // gameover
-							if (this.gameoverIntroAnimation(false)) this.setStep(100); // Restart game
+							if (this.gameoverIntroAnimation(false)) this.setState(100); // Restart game
 							break;
 						}
 						case 801:{ // Game ending
 							if (this.gameEndingIntroAnimation(false)) {
 								this._loselife={ending:true};
-								this.setStep(700); // Game over
+								this.setState(700); // Game over
 							}
 							break;
 						}
 					}
-					
 					this.hud.blit();
 					break;
 				}
 			}
 		}
-	  });
-	 }
+	});
 	}
+}
